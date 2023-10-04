367a5e52c8e42ce39f397d2cc8f63e9e95582b0b
Aleksandr Belousov <lksndrbelousov1983@mail.ru>
2023-10-04 12:51:11 +0700

    Second commit

--- src/test/java/ru/netology/services/CalcMonthRestServiceTest.java
+++ src/test/java/ru/netology/services/CalcMonthRestServiceTest.java
@@ -1,29 +1,22 @@
 package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalcMonthRestServiceTest {
 
-    @Test
-    public void testCalculateWhenThreeMonthRestInYear() {
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+    public void testCalculateWhatMonthRestInYear(int expected, int income, int expenses, int threshold) {
         CalcMonthRestService service = new CalcMonthRestService();
 
-        int actual = service.calculate(10_000, 3_000, 20_000);
-        int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void testCalculateWhenTwoMonthRestInYear() {
-        CalcMonthRestService service = new CalcMonthRestService();
-
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        int expected = 2;
-
-        Assertions.assertEquals(expected, actual);
-
 
     }
 }
